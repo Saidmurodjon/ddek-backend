9f55527064f9597b47dcee7544cb34112babaf94
saidmurod <saifdmurodjon1020@gmail.com>
2023-02-01 22:12:15 +0500

    auth

--- src/api/v1/auth/Router.js
+++ src/api/v1/auth/Router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("./controller");
+const authController = require("./Controller");
 
 router.route("/").post(authController.Login);
 
